45ccf0df13ab3c6b60d7bcee62634d24794538c0
Wanderer-IT <godfrediandublin89@gmail.com>
2026-04-10 10:09:27 +0800

    initialize

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-// run `node index.js` in the terminal
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const students = [
+    {
+        id: 1,
+        name: "Wanderer",
+        yearLevel: 1,
+    },
+    {
+        id: 2,
+        name: "Shin",
+        yearLevel: 2,
+    },
+    {
+        id: 3,
+        name: "Acheron",
+        yearLevel: 3,
+    },
+]
+
+app.get('/api/students', (req, res) =>{
+    res.json(students)
+})
+
+app.post('/api/students', (req, res) =>{
+    const {name,yearLevel} = req.body
+    const newStudent = {name, yearLevel}
+    students.push(newStudent)
+
+    res.json(201).json({
+        message: "Student added successfuly",
+        students:newStudent
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:no${port}`)
+})
